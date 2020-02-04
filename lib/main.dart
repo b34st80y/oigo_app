b3dfe4c3f8cca65e9cde2eae49f646ea127defb7
b34st80y <mailadamg@gmail.com>
2020-02-04 17:43:07 -0500

    change routing via BottomNavigationBar instead of Drawer.

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:oigo_app/Pages/AlliancePage.dart';
 import 'package:oigo_app/Pages/EmotionsPage.dart';
-import 'package:oigo_app/Pages/HomePage.dart';
 import 'package:oigo_app/Pages/SettingsPage.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Oigo App',
-    home: HomePage(),
-    initialRoute: '/',
-    routes: {
-      //'/': (context) => HomePage(),
-      '/emotions': (context) => EmotionsPage(),
-      '/alliance': (context) => AlliancePage(),
-      '/settings': (context) => SettingsPage(),
-    },
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
 }
 
-class CustomListTile extends StatelessWidget {
-  IconData icon;
-  String text;
-  Function onTap;
+class MyAppState extends State<MyApp> {
 
-  CustomListTile(this.icon, this.text, this.onTap);
+  int _selectedPage = 0;
+  final _pageOptions = [
+    EmotionsPage(),
+    AlliancePage(),
+    SettingsPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-      child: Container(
-        decoration: BoxDecoration(
-            border: Border(bottom: BorderSide(color: Colors.grey))),
-        child: InkWell(
-          onTap: () {onTap();},
-          child: Container(
-            height: 50,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(children: <Widget>[
-                  Icon(icon),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      text,
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ),
-                ]),
-                Icon(Icons.arrow_right),
-              ],
+    return MaterialApp(
+      title: "Hermes Tracker",
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hermes Tracker')),
+        body: _pageOptions[_selectedPage],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedPage,
+          onTap: (int index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.face),
+              title: Text('Emotions')
             ),
-          ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.people),
+                title: Text('Alliance')
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text('Settings')
+            ),
+          ],
         ),
-      ),
+      )
     );
   }
 }
