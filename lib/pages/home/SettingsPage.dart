3498ae6b91ec1e16a36aa713f85d8abf0ed46236
adamghaffarian <mailadamg@gmail.com>
2020-03-26 18:25:31 -0400

    moved about widget to proper location

--- lib/pages/home/SettingsPage.dart
+++ lib/pages/home/SettingsPage.dart
@@ -3,7 +3,7 @@ import 'package:oigo_app/services/auth.dart';
 import 'package:oigo_app/pages/home/settings/Help.dart';
 import 'package:oigo_app/pages/home/settings/Notifications.dart';
 import 'package:oigo_app/pages/home/settings/Alliance.dart';
-import 'package:oigo_app/pages/settings/About.dart';
+import 'package:oigo_app/pages/home/settings/About.dart';
 
 final AuthService _auth = AuthService();
 
@@ -11,34 +11,28 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text('Settings'),
-          actions: <Widget>[
-            FlatButton.icon(
-                icon: Icon(Icons.lock),
-                label: Text('Logout'),
-                onPressed: () {
-                  _auth.signOut();
-                  }),
-        ]),
-
+      appBar: AppBar(title: Text('Settings'), actions: <Widget>[
+        FlatButton.icon(
+            icon: Icon(Icons.lock),
+            label: Text('Logout'),
+            onPressed: () {
+              _auth.signOut();
+            }),
+      ]),
       body: ListView(
         //physics: BouncingScrollPhysics(),
         children: <Widget>[
           SettingsInkwell(
             text: "Account",
             icon: Icon(Icons.settings),
-            onTap: () {
-
-            },
+            onTap: () {},
           ),
           SettingsInkwell(
             icon: Icon(Icons.settings),
             text: "Alliance",
             onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => Alliance()));
-
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Alliance()));
             },
           ),
           SettingsInkwell(
@@ -47,26 +41,22 @@ class SettingsPage extends StatelessWidget {
             onTap: () {
               Navigator.of(context).push(
                   MaterialPageRoute(builder: (context) => Notifications()));
-
             },
           ),
           SettingsInkwell(
-            icon: Icon(Icons.settings),
-            text: "About",
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => About()));
-            }
-          ),
+              icon: Icon(Icons.settings),
+              text: "About",
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => About()));
+              }),
           SettingsInkwell(
-            icon: Icon(Icons.settings),
-            text: "Help",
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => Help()));
-
-            }
-          )
+              icon: Icon(Icons.settings),
+              text: "Help",
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Help()));
+              })
         ],
       ),
     );
@@ -74,7 +64,6 @@ class SettingsPage extends StatelessWidget {
 }
 
 class SettingsInkwell extends StatelessWidget {
-
   final String text;
   final Function onTap;
   final Icon icon;
@@ -84,7 +73,8 @@ class SettingsInkwell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(border: Border(bottom: BorderSide(width: .8, color: Colors.grey))),
+      decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(width: .8, color: Colors.grey))),
       child: InkWell(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -95,11 +85,16 @@ class SettingsInkwell extends StatelessWidget {
                 padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
                 child: icon,
               ),
-              Text(text,
+              Text(
+                text,
                 style: TextStyle(
                   fontSize: 25,
-                ),),
-              Flexible(child: Container(alignment: Alignment.centerRight,child: Icon(Icons.arrow_forward_ios))),
+                ),
+              ),
+              Flexible(
+                  child: Container(
+                      alignment: Alignment.centerRight,
+                      child: Icon(Icons.arrow_forward_ios))),
             ],
           ),
         ),
