a4eb8dd1416373919500d701696f7f8cc58e6ca2
adamghaffarian <mailadamg@gmail.com>
2020-03-26 21:17:52 -0400

    moved about widget to proper location

--- lib/pages/home/AlliancePage.dart
+++ lib/pages/home/AlliancePage.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:oigo_app/datatypes/message.dart';
 import 'package:oigo_app/widgets/chat_bubble.dart';
 import 'package:oigo_app/widgets/loading.dart';
@@ -20,6 +21,79 @@ class AlliancePage extends StatelessWidget {
             child: Text("Create New Alliance"),
             onPressed: () {
               DatabaseService(uid: user.uid).createAlliance();
+            },
+          ),
+          RaisedButton(
+            child: Text("Join Alliance"),
+            onPressed: () async {
+              String alliance = "";
+
+              TextEditingController _textFieldController = TextEditingController();
+
+              _displayDialog(BuildContext context) async {
+                return showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('TextField in Dialog'),
+                        content: TextField(
+                          controller: _textFieldController,
+                          decoration: InputDecoration(hintText: "TextField in Dialog"),
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('Join'),
+                            onPressed: () {
+                              alliance = _textFieldController.text;
+                              DatabaseService().updateUserAlliance(alliance);
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    });
+              }
+              await _displayDialog(context);
+            },
+          ),
+          RaisedButton(
+            child: Text("Share Alliance Code"),
+            onPressed: () async {
+              String alliance = await DatabaseService(uid: user.uid).getAlliance();
+
+              _displayDialog(BuildContext context) async {
+                return showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Alliance Code'),
+                        content: SelectableText(alliance),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('Copy'),
+                            onPressed: () {
+                              Clipboard.setData(ClipboardData(text: alliance));
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    });
+              }
+              _displayDialog(context);
+
             },
           ),
           RaisedButton(
@@ -47,7 +121,37 @@ class AlliancePage extends StatelessWidget {
                                 text: messageSnapshot.data['message'],
                                 sender: messageSnapshot.data['sender'],
                                 timestamp: messageSnapshot.data['timestamp']);
-                            return ChatBubble(message: message);
+                            if (message.sender == user.uid) {
+                              return Row(
+                                children: <Widget>[
+                                  Flexible(
+                                    child: Container(
+                                      color: Colors.transparent,
+                                    ),
+                                    flex: 7,
+                                  ),
+                                  Flexible(
+                                    child: ChatBubbleRight(message: message),
+                                    flex: 6,
+                                  ),
+                                ],
+                              );
+                            } else {
+                              return Row(
+                                children: <Widget>[
+                                  Flexible(
+                                    child: ChatBubbleLeft(message: message),
+                                    flex: 6,
+                                  ),
+                                  Flexible(
+                                    child: Container(
+                                      color: Colors.transparent,
+                                    ),
+                                    flex: 7,
+                                  ),
+                                ],
+                              );
+                            }
                           }),
                     );
                   } else {
--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:oigo_app/datatypes/message.dart';
 import 'package:provider/provider.dart';
 
-class ChatBubble extends StatelessWidget {
+class ChatBubbleRight extends StatelessWidget {
   final Message message;
 
-  ChatBubble({this.message});
+  ChatBubbleRight({this.message});
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +30,43 @@ class ChatBubble extends StatelessWidget {
               color: Colors.black54,
             )
           ],
-          gradient: message.sender == message.sender
-              ? LinearGradient(
+          gradient: LinearGradient(
             colors: [Colors.lightBlueAccent, Colors.blue],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-          )
-              : LinearGradient(
+          )),
+    );
+  }
+}
+
+class ChatBubbleLeft extends StatelessWidget {
+  final Message message;
+
+  ChatBubbleLeft({this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<FirebaseUser>(context);
+    return Container(
+      alignment: Alignment.centerRight,
+      child: Text(message.text),
+      //constraints: BoxConstraints(maxWidth: 50), //WHY DOESNT THIS WORK???
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
+            bottomRight: Radius.circular(15),
+          ),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 10.0,
+              offset: Offset(5, 5),
+              color: Colors.black54,
+            )
+          ],
+          gradient: LinearGradient(
             colors: [Colors.greenAccent, Colors.green],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
